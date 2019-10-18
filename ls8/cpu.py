4c097fe7c88dd91e29b55dac1b309596ad0b5346
rmejia-tajc <rmejia.tajc@gmail.com>
2019-10-18 11:04:19 -0600

    add 'JMP', 'JEP', and 'JNE' instructions

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -15,6 +15,9 @@ CALL = 80  # 0b01010000
 RET = 17   # 0b00010001
 ADD = 160  # 0b10100000
 CMP = 167  # 0b10100111
+JMP = 84   # 0b01010100
+JEQ = 85   # 0b01010101
+JNE = 86   # 0b01010110
 
 class CPU:
     """Main CPU class."""
@@ -211,19 +214,51 @@ class CPU:
                 self.reg[operand_a] = self.reg[operand_a] + self.reg[operand_b]
                 self.pc += 3
 
-            elif ir == CMP:
+            elif ir == CMP: # Compare the values in two registers
                 print("CMP")
+                # If they are equal, set the Equal `E` flag to 1
                 if self.reg[operand_a] == self.reg[operand_b]:
                     print("EQUALS")
                     self.reg[self.fl] = 0b00000001
+                # If registerA is less than registerB, set the Less-than `L` flag to 1
                 elif self.reg[operand_a] < self.reg[operand_b]:
                     print("LESS THAN")
                     self.reg[self.fl] = 0b00000100
+                # If registerA is greater than registerB, set the Greater-than `G` flag to 1
                 elif self.reg[operand_a] > self.reg[operand_b]:
                     print("GREATER THAN")
                     self.reg[self.fl] = 0b00000010
+
                 self.pc += 3
-                
+
+            # Jump to the address stored in the given register
+            elif ir == JMP:
+                print("JMP")
+                # Set the `PC` to the address stored in the given register
+                self.pc = self.reg[operand_a]
+
+
+            elif ir == JEQ:
+                print("JEQ")
+                # If `equal` flag is set (true), jump to the address stored in the given register
+                if self.reg[self.fl] == 0b00000001:
+                    print("JEQ True")
+                    self.pc = self.reg[operand_a]
+
+                else:
+                    print("JEQ False")
+                    self.pc += 2
+
+
+            elif ir == JNE:
+                print("JNE")
+                # If `E` flag is clear (false, 0), jump to the address stored in the given register
+                if self.reg[self.fl] != 0b00000001:
+                    print("JNE True")
+                    self.pc = self.reg[operand_a]
+                else:
+                    print("JNE False")
+                    self.pc += 2
 
 
 
